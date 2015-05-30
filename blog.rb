76ba2c38276565b425b4d5d6dd0552e619b3c046
Adrian Cuadros <adrian@innku.com>
2015-05-30 15:27:15 -0500

    Adds a template to blog

--- blog.rb
+++ blog.rb
@@ -1,8 +1,20 @@
 require 'rubygems'
+require 'erb'
 require 'github/markup'
 
 class Blog
+  LAYOUT = './views/layout.erb'
+
   def render
-    GitHub::Markup.render('welcome.markdown', File.read('./posts/welcome.markdown'))
+    render_template do
+      GitHub::Markup.render('welcome.markdown', File.read('./posts/welcome.markdown'))
+    end
   end
+
+  private
+
+  def render_template
+    ERB.new(File.read(LAYOUT)).result(binding)
+  end
+
 end
